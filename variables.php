b647743f245689316eebe36bfa0fdef907f29fda
m0zs0 <monoki.zsolt@moriczref.hu>
2025-09-04 10:34:37 +0200

    variables.php

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php
+    /*
+    Szükséges programok: XAMPP (Apache, MySQL), Git, github repo, VSC
+    <?php ...   ?>
+    Változók
+
+    */
+
+    $name = "mozso";
+    $age = 18;
+    $city = "Kisújszállás";
+    var_dump($name);
+
+    echo "\n<br>\nNév: $name Életkor: $age Lakóhely: $city";
+?>
