2ba789a2fd7066cbdf3aac6062d30e16e3da6fe2
m0zs0 <monoki.zsolt@moriczref.hu>
2025-09-08 08:46:28 +0200

    if done

--- structures.php
+++ structures.php
@@ -1,9 +1,23 @@
 <?php
     /*
     1. if, else, elseif
-    switch
-    Ciklusok: for, while, foreach
-    Ternary operator
-    Tömbök (indexelt, asszociatív, tömbök tömbje)
+    2. switch
+    3. Ciklusok: for, while, foreach
+    4. Ternary operator ?:
+    5. Tömbök (indexelt, asszociatív, tömbök tömbje)
     */
+    // egy számról döntsd el h az páros e
+    $number = 6;
+    echo "A(z) {$number} egy ";
+    if ($number % 2 == 0) {
+        echo "páros";
+    } else { 
+        echo "páratlan";
+    }
+    echo " szám.<br>";
+
+    $result = ($number % 2 == 0) ? "páros" : "páratlan";
+    echo "A(z) $number egy $result szám.";
+    
+
 ?>
