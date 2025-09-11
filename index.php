adcddbfb1a101b537742608ac3ef2f63c4d6e94c
m0zs0 <monoki.zsolt@moriczref.hu>
2025-09-11 11:06:01 +0200

    index.php done

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-    echo "Szia";
+    require 'App/Traits/GreetingTrait.php';
+    require 'App/Traits/LoggerTrait.php';
+    require 'App/Services/MyService.php';
+
+    use App\Services\MyService;
+
+    $service = new MyService();
+    service -> run();
 ?>
